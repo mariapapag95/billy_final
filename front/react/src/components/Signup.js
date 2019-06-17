ce7dabf5cb9f7bd4f6bfb7e1e25292b2443c6b6d
Maria Papageorgiou <mpapag1995@gmail.com>
2019-06-17 18:51:23 -0400

    signup and login

--- front/react/src/components/Signup.js
+++ front/react/src/components/Signup.js
@@ -1,41 +1,70 @@
-import React from 'react'
-import Dash from './Dash';
+import React, {Component} from 'react'
+import { Redirect, Switch } from 'react-router-dom'
 
-function dash() {
-    return (<Dash/>)
-}
+class Signup extends Component {
+    state = {
+        signup : undefined,
+        errorMessage : ""
+    }
 
-function Signup() {
-    return(
-        <div>
-            <form>
-                <input 
-                    className = "input"
-                    id = "username"
-                    placeholder = "Username"
-                >
-                </input>
-                <input 
-                    className = "input"
-                    id = "password"
-                    placeholder = "Password"
-                >
-                </input>
-                <input 
-                    className = "input"
-                    id = "confirm"
-                    placeholder = "Confirm Password"
-                >
-                </input>
-                <button 
-                    className = "paybutton"
-                    onClick = {()=>{dash()}}
-                >
-                    SIGNUP
-                </button>
-            </form>
-        </div>
-    )
+    signup = () => {
+        this.setState({signup : true})
+    }
+
+    badsignup = () => {
+        this.setState({signup : false})
+    }
+
+    redirect () {
+        console.log("HELO")
+        return <Switch><Redirect from='/signup' to='/username'/></Switch>
+    }
+
+    refresh () {
+        console.log("signup = false")
+        return null
+    }
+    
+    render () {
+        return(
+            <div>
+                <form>
+                    <input 
+                        className = "input"
+                        id = "username"
+                        placeholder = "Username"
+                    >
+                    </input>
+                    <input 
+                        className = "input"
+                        id = "password"
+                        placeholer = "Password"
+                    >
+                    </input>
+                    <input 
+                        className = "input"
+                        id = "confirm"
+                        placeholer = "Confirm Password"
+                    >
+                    </input>
+                    <button 
+                        className = "paybutton"
+                        onClick = {()=>{this.signup()}}
+                    >
+                        signup SUCESSFUL
+                    </button>
+                    <button 
+                        className = "paybutton"
+                        onClick = {()=>{this.badsignup()}}
+                    >
+                        signup UNSUCESSFUL
+                    </button>
+                </form>
+                    {this.state.signup===true ? 
+                        (this.redirect()) : (this.refresh())}
+            </div>
+        )
+    }
 }
 
 export default Signup
